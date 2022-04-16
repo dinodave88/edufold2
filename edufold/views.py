690abca444bef0f4fdedd8b9b7593251be3d967a
dinodave88 <dinodave88@gmail.com>
2022-04-16 16:49:14 +0530

    try 5

--- edufold/views.py
+++ edufold/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "edufold/index.html")
+    return render(request, 'edufold/index.html')
+
+def about(request):
+    return render(request, 'edufold/about.html')
+
